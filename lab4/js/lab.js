fd3e21236f708ba4a4b632211cebe2882bc22648
Juliet Gemmell <jegemmel@ucsc.edu>
2022-10-12 11:38:28 -0700

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+//Create Area Border
+//Create Food Pixel
+  //respawns in random location when eaten
+//Create Snake Pixel
+  //Grows when eating
+  //Constantly moving in direction by player indication
+  //Snake freezes when it touches border or it's own body

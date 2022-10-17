9b45ddda45868fe86ce703ebf282b21068d3da42
Juliet Gemmell <jegemmel@ucsc.edu>
2022-10-17 11:37:42 -0700

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Juliet Gemmell
+ * Created:   10.17.2022
+ *
+ * License: Public Domian
+ **/
+
+ // Car Variables
+ make = "Ford";
+ model = "Hybrid Escape";
+ color = "Light Green";
+ year = 2008;
+
+ //Math
+ age = 2022 - year;
+
+ //Print Script
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "years<br>");

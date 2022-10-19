61742696ce70b21f06db272aa04ac23ddee76be5
Juliet Gemmell <jegemmel@ucsc.edu>
2022-10-19 12:05:27 -0700

    Update lab.js

---
+++ lab6/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Juliet Gemmell
+ * Created:   10.19.2022
+ *
+ * License: Public Domian
+ **/
+
+//Transportation Variables
+ myTansportation = ["car", "bus", "feet"];
+
+ myMainRide = {
+   make: "Ford",
+   model: "Hybrid Escape",
+   color: "Light Green",
+   year: 2008,
+   Age: Function(){
+     return 2022 - Age;
+   }
+ }
+
+ 

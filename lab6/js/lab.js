6e6151e8fa3021eb73785aeb47733f7065788d0d
Juliet Gemmell <jegemmel@ucsc.edu>
2022-10-20 12:48:17 -0700

    Update lab.js

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -5,19 +5,19 @@
  **/
 
 //Transportation Variables
- myTransportation = [ "car", "bus", "feet" ];
+myTransportation = [ "car", "bus", "feet" ];
 
- myMainRide = {
-   make: "Ford",
-   model: "Hybrid Escape",
-   color: "Light Green",
-   year: 2008,
-   age: function(){
-     return 2022 - year;
-   }
+myMainRide = {
+  make: "Ford",
+  model: "Hybrid Escape",
+  color: "Light Green",
+  year: 2008,
+  age: function(){
+    return 2022 - year;
+  }
  }
 space = {};
 
  //Print Script
- document.writeln("Getting around: ", myTransportation);
- document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
+document.writeln("Getting around: ", myTransportation);
+document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");

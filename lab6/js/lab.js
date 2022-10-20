ac1da0fe0ea889da0713e9eb7cf57fbfe0ecaa82
Juliet Gemmell <jegemmel@ucsc.edu>
2022-10-19 17:41:50 -0700

    Update lab.js

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -19,6 +19,6 @@
  }
 
  //Print Script
- document.writeln("The Kinds of Transportation I use: ", myTansportation, "</br>");
+ document.writeln("Getting around: " + myTransport + "<br>");
  document.writeln("My Main Ride: <pre>",
      JSON.stringify(myMainRide, null, '\t'), "</pre>");

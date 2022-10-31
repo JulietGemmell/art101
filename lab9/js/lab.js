9cdfc507162f887b03cafd08f89d2dc4cf65d443
Juliet Gemmell <jegemmel@ucsc.edu>
2022-10-31 11:48:41 -0700

    css edits

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,5 +1,19 @@
 /**
  * Author: Juliet Gemmell
- * Created: 10.26.2022
+ * Created: 10.31.2022
  * License: Public Domian
  **/
+
+var outputEL = document.getElementById("Output");
+
+var new1EL = document.createElement("p");
+var new2EL = document.createElement("p");
+
+new1EL.innerHTML="I like money";
+new2EL.innerHTML="I eat coins off the street";
+
+outputEL.appendChild(new1EL);
+outputEL.appendChild(new2EL);
+
+new1EL.style.fontFamily="Helvetica";
+new2EL.style.fontSize="40px";

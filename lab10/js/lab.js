72d2ab85941589bf4852b98ec7b010c039933131
Juliet Gemmell <jegemmel@ucsc.edu>
2022-11-02 22:45:09 -0700

    lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -3,3 +3,27 @@
  * Created: 11.02.2022
  * License: Public Domian
  **/
+
+ function nameSorter(inputName){
+   var userName = inputName;
+
+   var nameArray = userName.split('');
+
+   var sortedArray = nameArray.sort();
+
+   var sortedName = sortedArray.join('');
+
+   return (sortedName);
+
+ }
+
+   var buttonEl = document.getElementById("my-button")
+
+   var outputEl = document.getElementById("output");
+
+   buttonEl.addEventListener('click', function(){
+     var inputVal = document.getElementById("user-name").value;
+     var sortedNewName = nameSorter(inputVal);
+     outputEl.innerHTML = sortedNewName;
+     document.getElementById("user-name").value="hey stupid";
+   })

8adc5ce732e7594c151b80ebb983f32de75d6517
Juliet Gemmell <jegemmel@ucsc.edu>
2022-11-07 12:23:07 -0800

    lab 11 part1

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,5 +1,23 @@
 /**
  * Author: Juliet Gemmell
- * Created: 10.26.2022
+ * Created: 11.07.2022
  * License: Public Domian
  **/
+$('#challenge').append('<button id=buttonChalEL> click me.</button>');
+
+$('#problems').append('<button id=buttonProbEL> I want to break free</button>');
+
+$('#results').append('<button id=buttonResEL> CLICK HERE! </button>');
+
+
+$('#buttonChalEL').click(function(){
+   $('#challenge').toggleClass('specialler');
+});
+
+$('#buttonProbEL').click(function(){
+  $('#problems').toggleClass('specialler');
+});
+
+$('#buttonResEL').click(function(){
+  $('#results').toggleClass('specialler');
+});

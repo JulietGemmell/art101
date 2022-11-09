5eff590b667e597852e0e163ac76a0239f02ef8a
Juliet Gemmell <jegemmel@ucsc.edu>
2022-11-09 11:34:26 -0800

    lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,5 +1,31 @@
 /**
  * Author: Juliet Gemmell
- * Created: 10.26.2022
+ * Created: 11.09.2022
  * License: Public Domian
  **/
+
+function sortingHat(str){
+  len = str.length;
+  mod = len % 4;
+
+  if (mod == 0){
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else if (mod == 3) {
+    return "Hufflepuff"
+  }
+}
+
+var myButton = document.getElementById('button');
+myButton.addEventListener("click", function(){
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  var newText = "The Sorting Hat has sorted you into " + house;
+  document.getElementById("output").innerHTML = newText
+})

d362efa589166eb7391268495b0ec599b8232680
Juliet Gemmell <jegemmel@ucsc.edu>
2022-11-14 14:46:22 -0800

    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,5 +1,31 @@
 /**
  * Author: Juliet Gemmell
- * Created: 10.26.2022
+ * Created: 11.14.2022
  * License: Public Domian
  **/
+
+ function fizzBuzz(){
+
+  for (i=1; i <= 200; i++) {
+    var newStr = "";
+    if (i % 3 == 0) {
+      newStr = newStr + "Fizz";
+      }
+    if (i % 5 == 0) {
+      newStr = newStr + "Buzz";
+    }
+    if (i % 7 == 0 ) {
+      newStr = newStr + "Boom";
+    }
+    if (newStr.length > 0) {
+      $("#output").append("<p> "+newStr+" </p>");
+    }
+    else{
+      $("#output").append("<p> "+i+" </p>");
+    }
+  }
+}
+
+$("#submit").click(function(){
+  fizzBuzz();
+});

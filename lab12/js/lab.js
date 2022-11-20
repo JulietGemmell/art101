87027659a7d0a61ff783479eabb97557da352fae
Juliet Gemmell <jegemmel@ucsc.edu>
2022-11-20 12:45:32 -0800

    Update lab.js

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -24,8 +24,8 @@ function sortingHat(str){
 
 var myButton = document.getElementById('button');
 myButton.addEventListener("click", function(){
-  var name = document.getElementById("input").value;
+  var name = document.getElementById("input").value
   var house = sortingHat(name);
-  var newText = "The Sorting Hat has sorted you into " + house;
+  newText = "The Sorting Hat has sorted you into " + house;
   document.getElementById("output").innerHTML = newText
 })

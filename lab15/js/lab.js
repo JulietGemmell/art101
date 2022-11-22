07757f4c0ae22cfae5ba1b75ec11be0b9c1db106
Juliet Gemmell <jegemmel@ucsc.edu>
2022-11-21 20:11:38 -0800

    cant figureout parse

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,5 +1,33 @@
 /**
  * Author: Juliet Gemmell
- * Created: 10.26.2022
+ * Created: 11.21.2022
  * License: Public Domian
  **/
+
+JSON.stringify(data)
+ // Using the core $.ajax() method
+ $.ajax({
+     // The URL for the request (from the api docs)
+     url: "https://yesno.wtf/#api",
+     // The data to send (will be converted to a query string)
+     data: {
+       "answer": "yes",
+       "forced": false,
+       "image": "https://yesno.wtf/assets/yes/2.gif"
+     },
+     // Whether this is a POST or GET request
+     type: "GET",
+     // The type of data we expect back
+     dataType : "json",
+     // What do we do when the api call is successful
+     //   all the action goes in here
+     success: function(data) {
+         // do stuff
+         console.log(data);
+     },
+     // What we do if the api call fails
+     error: function (jqXHR, textStatus, errorThrown) {
+         // do stuff
+         console.log("Error:", textStatus, errorThrown);
+     }
+ })

26a9fdbaaffbd5c5a8bdf19659d4770de3f1ad09
Juliet Gemmell <jegemmel@ucsc.edu>
2022-11-24 17:53:55 -0800

    Update lab.js

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -3,3 +3,38 @@
  * Created: 11.23.2022
  * License: Public Domian
  **/
+
+ // Using the core $.ajax() method
+ $.ajax({
+     // The URL for the request (from the api docs)
+     url: "https://xkcd.com/",
+     // The data to send (will be converted to a query string)
+     data: {
+       "month": "11",
+       "num": 2702,
+       "link": "https://xkcd.com/what-if-2",
+       "year": "2022",
+       "news": "",
+       "safe_title": "What If 2 Gift Guide",
+       "transcript": "",
+       "alt": "BABIES OR LITERATURE BUT NOT BOTH: Baby shoes",
+       "img": "https://imgs.xkcd.com/comics/what_if_2_gift_guide.png",
+       "title": "What If 2 Gift Guide",
+       "day": "23"
+     },
+     // Whether this is a POST or GET request
+     type: "GET",
+     // The type of data we expect back
+     dataType : "json",
+     // What do we do when the api call is successful
+     //   all the action goes in here
+     success: function(data) {
+         // do stuff
+         console.log(data);
+     },
+     // What we do if the api call fails
+     error: function (jqXHR, textStatus, errorThrown) {
+         // do stuff
+         console.log("Error:", textStatus, errorThrown);
+     }
+ })
